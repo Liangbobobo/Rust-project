dbcc5a90679b3dbb189601b23e59ed569b6a1ab5
liang <liangshibo@163.com>
2026-05-20 17:59:40 +0800

    hypnus

---
+++ RustRedOps/三件套/uwd-mariana/mariana/src/lib.rs
@@ -0,0 +1,12 @@
+#![no_std]
+#![allow(
+    clippy::missing_transmute_annotations, 
+    clippy::useless_transmute,
+    clippy::collapsible_if,
+    non_snake_case, // 变量/字段名警告
+    non_camel_case_types, // 类型名警告
+    non_upper_case_globals // 全局变量/常量名警告
+)]
+
+
+pub mod uwd;
